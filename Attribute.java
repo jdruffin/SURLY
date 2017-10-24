ec87d2e474ac3967c035fe09927db94851bab80a
Ruffin <@wwu.edu>
2017-10-24 13:40:03 -0700

    comments for surly and attribute classes, very close to compiling

--- Attribute.java
+++ Attribute.java
@@ -1,10 +1,23 @@
 public class Attribute{
 
 	private String name;
+	//private String type;
+	//private int length;
+	//private String value;
+
 
 	//constructor for player object
-	public Attribute(String name){
+	public Attribute(String name){ /*String type, int length, String value*/
 		this.name = name;
+		//this.type = type;
+		//this.length = length;
+		//this.value = value;
 	}
-	
+
+	//public static void checkValid(){
+		//ifgood
+		//reformat if need be
+		//if bad print" bad attribute
+	//}
+
 }
--- Surly.java
+++ Surly.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Surly{
 
-  public static LinkedList<Relation> relationList   = new LinkedList<Relation>();
+  public static LinkedList<Relation> relationList = new LinkedList<Relation>();
 
   public static void main(String args[]){
     try{
@@ -25,7 +25,7 @@ public class Surly{
             LinkedList<Tuple> tupleList         = new LinkedList<Tuple>();
 
             for(int i = 2; i < parts.length; i = i+3){
-              Attribute attribute = new Attribute(parts[i]);
+              Attribute attribute = new Attribute(parts[i]/*,parts[i+1],parts[i+2], NULL*/);
               attributeList.add(attribute);
             }
 
@@ -45,10 +45,11 @@ public class Surly{
             for (int i = 0; i < relationList.size(); i++) {
               if(relationList.get(i).name.equals(parts[1])){
                 for(int j = 2; j < parts.length; j++){
-                  Attribute attributeInsert = new Attribute(parts[j]);
+                  Attribute attributeInsert = new Attribute(parts[j]/*,parts[j+1],parts[j+2], NULL*/);
+                  //Attribute.checkValid();
                   attributeList.add(attributeInsert);
                 }
-
+                //ISSUE WITH '' IN TEST.TXT
                 Tuple tupleInsert = new Tuple(attributeList);
                 relationList.get(i).relation.add(tupleInsert);
 
@@ -62,7 +63,7 @@ public class Surly{
             for (int i = 0; i < relationList.size(); i++){
               for(int j = 1; j < parts.length; j++){
                 if(relationList.get(i).name.equals(parts[j])){
-                  System.out.print(relationList.get(i).toString()+"\n");
+                  System.out.print(relationList.get(i).name.toString()+"\n");
                 }
 
               }
@@ -77,6 +78,11 @@ public class Surly{
     } catch(FileNotFoundException e){
       System.out.print("No file found");
     }
+
+    /*take commands from cmd prompt
+    while(true){
+      system.in
+    }*/
 
  }
 
