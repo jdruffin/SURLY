f28815ba69027bd7f941200f1b8a9bfa87f17c07
Daniel Mark Martin Lorigan <lorigad@LabImage2016-17>
2017-12-01 13:25:57 -0800

    condition skeletons

--- conditionParser.java
+++ conditionParser.java
@@ -3,5 +3,23 @@ import java.util.regex.*;
 import java.lang.*;
 
 public class conditionParser{
-  
+
+  public boolean evalate(String[] conditions){
+
+  }
+
+  public boolean equals(relation, op1, op2){
+    if (relation.op1 == op2){
+      return true;
+    }
+  }
+
+  public boolean notEquals(){
+
+  }
+
+  public boolean or(relation, op1, op2){
+
+  }
+
 }
