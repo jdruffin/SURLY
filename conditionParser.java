2e8e5b2a6dfb282e2bd1cf044d4ee8242c51302a
Ruffin <@wwu.edu>
2017-12-06 12:23:10 -0800

    not workign

--- conditionParser.java
+++ conditionParser.java
@@ -4,22 +4,110 @@ import java.lang.*;
 
 public class conditionParser{
 
-  public boolean evalate(String[] conditions){
+  public static ArrayList<Boolean> createBools(Tuple tuple, String[] conditions){
+    ArrayList<Boolean> bools = new ArrayList<Boolean>();
+    boolean FALSE = new Boolean(false);
+    boolean TRUE = new Boolean(true);
 
-  }
+    for(int i = 1; i < conditions.length; i = i + 4){
+      for(Attribute a : tuple.getTuple()){
+        if(a.getName() == conditions[i]){
+          if(conditions[i+1].equals("=")){
+            if(a.getValue() == conditions[i+2]){
+              bools.add(TRUE);
+            }else{
+              bools.add(FALSE);
+            }
+          }
+          if(conditions[i+1].equals("!=")){
+            if(a.getValue() != conditions[i+2]){
+              bools.add(TRUE);
+            }else{
+              bools.add(FALSE);
+            }
+          }
+          if(conditions[i+1].equals(">")){
+            if(Double.parseDouble(a.getValue()) > Double.parseDouble(conditions[i+2])){
+              bools.add(TRUE);
+            }else{
+              bools.add(FALSE);
+            }
+          }
+          if(conditions[i+1].equals("<")){
+            if(Double.parseDouble(a.getValue()) < Double.parseDouble(conditions[i+2])){
+              bools.add(TRUE);
+            }else{
+              bools.add(FALSE);
+            }
+          }
 
-  public boolean equals(relation, op1, op2){
-    if (relation.op1 == op2){
-      return true;
+        }
+        }
+      }
+      return bools;
     }
+
+
+  public static ArrayList<String> createOperators(String[] conditions){
+    ArrayList<String> operators = new ArrayList<String>();
+    for(int i = 4; i < conditions.length; i=i+4){
+      operators.add(conditions[i]);
+    }
+    return operators;
   }
 
-  public boolean notEquals(){
+  public static boolean helper(ArrayList<String> operators, ArrayList<Boolean> bools){
+    //for(String s : operators){
+	     //System.out.print(s);
+    //}
+    for(int i = 0; i < operators.size(); i++){
+      if(operators.get(i).equals("and")){
+        boolean result = bools.get(i) && bools.get(i+1);
+        bools.remove(i+1);
+        bools.set(i, result);
+        operators.remove(i);
+        return helper(operators, bools);
+      }
+    }
+    for(int i = 0; i < bools.size(); i++){
+      if(bools.get(i) == true){
+        return true;
+      }
+    }
+
+  return false;
+  }
+
+  public static boolean evalate(Tuple tuple, String[] conditions){
+    ArrayList<String> operators = createOperators(conditions);
+    ArrayList<Boolean> bools    = createBools(tuple, conditions);
+	  System.out.print("asfasdfasd");
+    boolean result = helper(operators, bools);
+
+    return result;
 
   }
 
-  public boolean or(relation, op1, op2){
+//                     F
 
-  }
+
+
+//                    or
+//           T                       F
+
+
+
+//                     or                    and
+//           T                      T                 F
+
+
+
+
+
+//                       or                 and           and
+//            T                   T               T              F
+
+  //WHERE CNBR = CSCI241 or CNBR = CSCI145 and PNBR != CSCI141;
+
 
 }
